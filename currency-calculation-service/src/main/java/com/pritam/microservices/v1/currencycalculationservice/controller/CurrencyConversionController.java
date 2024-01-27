80535d1dfc0f188c6db626fbcbcfe802a4fcd66d
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-27 23:42:07 +0530

    Topic-18

--- currency-calculation-service/src/main/java/com/pritam/microservices/v1/currencycalculationservice/controller/CurrencyConversionController.java
+++ currency-calculation-service/src/main/java/com/pritam/microservices/v1/currencycalculationservice/controller/CurrencyConversionController.java
@@ -1,17 +1,33 @@
 package com.pritam.microservices.v1.currencycalculationservice.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.pritam.microservices.v1.currencycalculationservice.model.CurrencyConversionBean;
 
 @RestController
 public class CurrencyConversionController {
+
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean retrieveCurrencyConversionBean(@PathVariable String from, @PathVariable String to, @PathVariable double quantity) {
-		CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(1, from, to, 65.0, quantity, 8000);
-		currencyConversionBean.setTotalCalculatedAmount();
+	public CurrencyConversionBean retrieveCurrencyConversionBean(@PathVariable String from, @PathVariable String to,
+			@PathVariable double quantity) {
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+				uriVariables);
+		CurrencyConversionBean currencyConversionBean = responseEntity.getBody();
+		if (currencyConversionBean != null) {
+			currencyConversionBean.setQuantity(quantity);
+			currencyConversionBean.setTotalCalculatedAmount();
+		}
 		return currencyConversionBean;
 	}
 }
