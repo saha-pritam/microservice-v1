721b7299cc6eb94249fcbaeab5acc3982d267251
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-27 10:25:48 +0530

    Topic-7

---
+++ spring-cloud-config-server/src/test/java/com/pritam/microservices/v1/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v1.springcloudconfigserver;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
