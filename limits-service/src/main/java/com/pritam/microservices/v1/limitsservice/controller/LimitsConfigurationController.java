50e214eaab9abf74040e225fd0564c7055006ecc
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-30 01:08:01 +0530

    Topic-28

--- limits-service/src/main/java/com/pritam/microservices/v1/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/pritam/microservices/v1/limitsservice/controller/LimitsConfigurationController.java
@@ -1,6 +1,7 @@
 package com.pritam.microservices.v1.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import com.pritam.microservices.v1.limitsservice.model.LimitConfiguration;
 
 
 @RestController
+@RefreshScope
 public class LimitsConfigurationController {
 	
 	@Autowired
