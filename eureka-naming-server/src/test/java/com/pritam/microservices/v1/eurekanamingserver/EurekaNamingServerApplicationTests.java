05cd5c679d3ddcad030699fa33bf1e0522a94509
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-29 08:22:40 +0530

    Topic-21

---
+++ eureka-naming-server/src/test/java/com/pritam/microservices/v1/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v1.eurekanamingserver;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
