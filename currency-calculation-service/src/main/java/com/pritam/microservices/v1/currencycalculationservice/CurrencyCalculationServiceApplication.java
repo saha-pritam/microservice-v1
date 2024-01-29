fae30d41119a098e17cc7afff7537e1f691147dc
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-29 08:39:22 +0530

    Topic-23

--- currency-calculation-service/src/main/java/com/pritam/microservices/v1/currencycalculationservice/CurrencyCalculationServiceApplication.java
+++ currency-calculation-service/src/main/java/com/pritam/microservices/v1/currencycalculationservice/CurrencyCalculationServiceApplication.java
@@ -2,10 +2,12 @@ package com.pritam.microservices.v1.currencycalculationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.pritam.microservices.v1.currencycalculationservice")
+@EnableDiscoveryClient
 public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
