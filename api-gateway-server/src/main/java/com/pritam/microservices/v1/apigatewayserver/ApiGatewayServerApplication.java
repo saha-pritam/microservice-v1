126c29a4af5fffc560a5dc64e17a9267aafc3137
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-29 09:07:16 +0530

    Topic-24

---
+++ api-gateway-server/src/main/java/com/pritam/microservices/v1/apigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.pritam.microservices.v1.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ api-gateway-server/src/main/java/com/pritam/microservices/v1/apigatewayserver/filter/Filter.java
@@ -0,0 +1,39 @@
+package com.pritam.microservices.v1.apigatewayserver.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class Filter extends ZuulFilter {
+
+	private Logger logger = Logger.getLogger(Filter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("Request :- " + request + "\tURI :-" + request.getRequestURL());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
